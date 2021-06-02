5f4cd5c1e682cb1bc96d4b861fcdce9ed8fb2d98
Przemek Korona <przkor@gmail.com>
2021-06-02 10:59:17 +0200

    create form structure

--- src/App.js
+++ src/App.js
@@ -2,31 +2,120 @@ import logo from './logo.svg';
 import './App.css';
 import React,{useState}  from 'react'
 
-const dishes = [
-  {
-    name:'pizza'
-  },
-  {
-    name:'soup'
-  },
-  {
-    name:'sandwich'
-  }
-]
+const dishTypes = ['pizza','soup','sandwich']
 
 
 function App() {
 
-  const [number,setNumber] = useState()
+  const [number,setNumber] = useState(0)
+  const [name,setName] = useState('')
+  const [preparation,setPreparation]=useState('')
+  const [type,setType]=useState('')
+  const [dishDetails,setDishDetails] = useState('')
   
   const handleChangeNumber = (e) => {
-    setNumber(e.target.value)
+    setNumber(e.target.value) 
   }
+
+  const handleChangeDishName = (e) => {
+    setName(e.target.value) 
+  }
+
+  const handleChangePreparation = (e) => {
+    setPreparation(e.target.value) 
+  }
+
+  const handleSelectDishType = (e) => {
+    setType(e.target.value) 
+  }
+
+  const handleDishDetails = (e) => {
+    setDishDetails (e.target.value)
+  }
+
+
+  const types = dishTypes.map((dishType, index) => {
+    return <option key={index} value={dishType}>{dishType}</option>
+  })
+
+  const pizza = (
+    <p>pizza</p>
+  )
+
+  const soup = (
+    <p>soup</p>
+  )
+
+  const sandwitch = (
+    <p>sandwich</p>
+  )
+
+  const elementToShow = () => {
+    let element
+    switch (type) {
+        case 'pizza' : element=pizza
+        break
+        case 'soup' : element = soup
+        break
+        case 'sandwich' : element=sandwitch
+        break
+        default: return
+    }
+    return (  
+        <>
+        {element}
+        </>
+    );
+}
 
   const dishForm = () => {
    return (
       <form onSubmit="">
-        <div className="form-group">
+        <div>
+          <label htmlFor="name">Dish Name</label>
+          <input
+              type="text"
+              className="form-control"
+              id="name"
+              name="name"
+              value={name || ''}
+              onChange={handleChangeDishName}
+              required
+              />
+          <span class="validity"></span>
+        </div>
+        <div>
+          <label htmlFor="preparation_time">Preparation time</label>
+          <input 
+            type="time"
+            id="preparation_time"
+            name="preparation_time"
+            value={preparation || '00:00:00'}
+            onChange={handleChangePreparation}
+            required
+            />
+            <span class="validity"></span>
+        </div>
+        <div>
+          <label htmlFor="type">Dish type</label>
+          <select
+            onChange={handleSelectDishType}
+            className="form-control"
+            id="type"
+            name="type"
+            value={type || ''}
+            placeholder="wybierz"
+            required
+          >
+            <option value=''>wybierz</option>
+            {types}
+          </select>
+          <span class="validity"></span>
+        </div>
+        <div>
+          {type ? elementToShow() : '' }
+        </div>
+        <div>
             <label htmlFor="number">Slices</label>
             <input
               type="number"
@@ -34,11 +123,12 @@ function App() {
               id="number"
               name="number"
               value={number || ''}
-              step="1"
-              min="0"
+              step="0.1"
+              min="1"
               onChange={handleChangeNumber}
               required
               />
+            <span class="validity"></span>
           </div>
         </form>
     )
