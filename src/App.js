9ce15b876cfbf80810f11fa2badc203defc04fd1
Przemek Korona <przkor@gmail.com>
2021-06-02 09:57:23 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{useState}  from 'react'
+
+const dishes = [
+  {
+    name:'pizza'
+  },
+  {
+    name:'soup'
+  },
+  {
+    name:'sandwich'
+  }
+]
+
 
 function App() {
+
+  const [number,setNumber] = useState()
+  
+  const handleChangeNumber = (e) => {
+    setNumber(e.target.value)
+  }
+
+  const dishForm = () => {
+   return (
+      <form onSubmit="">
+        <div className="form-group">
+            <label htmlFor="number">Slices</label>
+            <input
+              type="number"
+              className="form-control"
+              id="number"
+              name="number"
+              value={number || ''}
+              step="1"
+              min="0"
+              onChange={handleChangeNumber}
+              required
+              />
+          </div>
+        </form>
+    )
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Dishes</h2>
       </header>
+      <content>
+        <p>Select</p>
+        {dishForm()}
+      </content>
     </div>
   );
 }
