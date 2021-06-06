d2b87ad616926dc63d26218478a45923f3992e54
Przemek Korona <przkor@gmail.com>
2021-06-06 09:59:32 +0200

    correct css style

--- src/App.js
+++ src/App.js
@@ -1,111 +1,16 @@
 import './App.css';
-import React,{useState}  from 'react'
 import DishForm from './components/DishForm'
-import axios from 'axios'
+
 
 function App() {
   
-  const [name,setName] = useState()
-  const [preparation_time,setPreparation_time]=useState()
-  const [type,setType]=useState()
-  const [dishDetails,setDishDetails] = useState({})
-
-  const [message,setMessage] = useState('')
- 
-  const handleChangeDishName = (e) => {
-    setName(e.target.value) 
-  }
-
-  const handleChangePreparation = (e) => {
-    setPreparation_time(e.target.value) 
-  }
-
-  const handleSelectDishType = (e) => {
-    setType(e.target.value) 
-    setDishDetails({})
-    setMessage('')
-  }
-
-  const handleDishDetails = (e) => {
-    if (e.target.name==='slices') {
-      setDishDetails(prevValue => {
-        return {
-          ...prevValue,
-          no_of_slices: parseInt(e.target.value),  
-        }
-      })
-    }
-    if (e.target.name==='diameter') {
-      setDishDetails(prevValue => {
-        return {
-          ...prevValue,
-          diameter: parseFloat(e.target.value),  
-        }
-      })
-    }
-    if (e.target.name==='spiciness_scale') {
-      setDishDetails(prevValue => {
-        return {
-          ...prevValue,
-          spiciness_scale: parseInt(e.target.value),  
-        }
-      })
-    }   
-    if (e.target.name==='slices_of_bread') {
-      setDishDetails(prevValue => {
-        return {
-          ...prevValue,
-          slices_of_bread : parseInt(e.target.value),  
-        }
-      })
-    }  
-  }
-
-  const handleSendDish = (e) => {
-    e.preventDefault()
-    const dishData = {
-      name:name,
-      preparation_time:preparation_time,
-      type:type
-    }
-    let data = Object.assign(dishData,dishDetails)
-    axios({
-      method: 'post',
-      url: 'https://frosty-wood-6558.getsandbox.com:443/dishes',
-      data: data
-    })
-    .then(function (response) {
-      if (response.status===200) {
-        alert('Dish was add')
-        setName('')
-        setPreparation_time('')
-        setType('')
-        setDishDetails({})
-        setMessage('')
-      }
-    })
-    .catch(function (error) {
-      Object.keys(error.response.data).forEach (key => {
-        setMessage(error.response.data[key])
-      })
-     console.log(error);
-    });
-  }
-
   return (
     <div className="App">
       <header className="App-header">
         <h2>Dishes</h2>
       </header>
       <section>
-        <DishForm name={name} preparation_time={preparation_time} type={type} 
-          dishDetails={dishDetails} message={message} 
-          handleChangeDishName={handleChangeDishName} 
-          handleChangePreparation={handleChangePreparation}
-          handleSelectDishType={handleSelectDishType}
-          handleDishDetails={handleDishDetails}
-          handleSendDish={handleSendDish}
-        />
+        <DishForm/>
       </section>
     </div>
   );
--- src/components/DishForm.jsx
+++ src/components/DishForm.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Pizza from './Pizza'
 import Soup from './Soup'
 import Sandwich from './Sandwich'
+import axios from 'axios'
 
 const dishTypes = ['pizza','soup','sandwich']
 
@@ -9,8 +10,94 @@ const types = dishTypes.map((dishType, index) => {
   return <option key={index} value={dishType}>{dishType}</option>
 })
 
-const DishForm = ({name,preparation_time,type,message,dishDetails,handleChangeDishName,
-    handleChangePreparation,handleSelectDishType,handleDishDetails,handleSendDish}) => {
+const DishForm = () => {
+
+  const [name,setName] = useState()
+  const [preparation_time,setPreparation_time]=useState()
+  const [type,setType]=useState()
+  const [dishDetails,setDishDetails] = useState({})
+
+  const [message,setMessage] = useState('')
+ 
+  const handleChangeDishName = (e) => {
+    setName(e.target.value) 
+  }
+
+  const handleChangePreparation = (e) => {
+    setPreparation_time(e.target.value) 
+  }
+
+  const handleSelectDishType = (e) => {
+    setType(e.target.value) 
+    setDishDetails({})
+    setMessage('')
+  }
+
+  const handleDishDetails = (e) => {
+    if (e.target.name==='slices') {
+      setDishDetails(prevValue => {
+        return {
+          ...prevValue,
+          no_of_slices: parseInt(e.target.value),  
+        }
+      })
+    }
+    if (e.target.name==='diameter') {
+      setDishDetails(prevValue => {
+        return {
+          ...prevValue,
+          diameter: parseFloat(e.target.value),  
+        }
+      })
+    }
+    if (e.target.name==='spiciness_scale') {
+      setDishDetails(prevValue => {
+        return {
+          ...prevValue,
+          spiciness_scale: parseInt(e.target.value),  
+        }
+      })
+    }   
+    if (e.target.name==='slices_of_bread') {
+      setDishDetails(prevValue => {
+        return {
+          ...prevValue,
+          slices_of_bread : parseInt(e.target.value),  
+        }
+      })
+    }  
+  }
+
+  const handleSendDish = (e) => {
+    e.preventDefault()
+    const dishData = {
+      name:name,
+      preparation_time:preparation_time,
+      type:type
+    }
+    let data = Object.assign(dishData,dishDetails)
+    axios({
+      method: 'post',
+      url: 'https://frosty-wood-6558.getsandbox.com:443/dishes',
+      data: data
+    })
+    .then(function (response) {
+      if (response.status===200) {
+        alert('Dish was add')
+        setName('')
+        setPreparation_time('')
+        setType('')
+        setDishDetails({})
+        setMessage('')
+      }
+    })
+    .catch(function (error) {
+      Object.keys(error.response.data).forEach (key => {
+        setMessage(error.response.data[key])
+      })
+     console.log(error);
+    });
+  }
 
     const elementToShow = () => {
       let element
@@ -87,7 +174,7 @@ const DishForm = ({name,preparation_time,type,message,dishDetails,handleChangeDi
          </div>
            {type ? elementToShow() : '' }
          <div></div>
-         <div>
+         <div className="formDiv">
            <span className="message">{message ? message : ''}</span>
            <button
              type="button"
